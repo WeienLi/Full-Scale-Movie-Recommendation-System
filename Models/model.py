0f9358a6fedefc2e69a8d5ec81fd7265ff763990
WeienLi <75823150+WeienLi@users.noreply.github.com>
2022-09-30 15:28:01 -0400

    Update model.py

--- Models/model.py
+++ Models/model.py
@@ -1,16 +1,34 @@
 # Import the model here
-from pyspark.ml.recommendation import ALSModel
+import numpy as np 
+import pandas as pd
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.ml.feature import StringIndexer
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType
+
+sc = SparkContext()
+spark = SparkSession.builder.appName('Recommendations').getOrCreate()
 
 def recommendMovies(userID):
-    ''' This function takes in a user ID and returns a list of 20 recommended movies for that user.'''
-    
-    # Load the model
+    #load model
     model = ALSModel.load("ALS")
-    # Get the recommendations
-    recommendations = model.recommendForAllUsers(10)
-    # Get the recommendations for the user
-    user_recommendations = recommendations.filter(recommendations.userId == userID)
-    # Get the movie names
-    movie_names = user_recommendations.select("recommendations.movieIndex").collect()
-    # Return the movie names
-    return movie_names
+    # load lookup table csv
+    lookup = spark.read.csv("lookuptable",sep = ',', header = True)
+    userid_int = int(userID)
+    user_subset = spark.createDataFrame([userid_int], IntegerType())
+    user_subset = user_subset. \
+        withColumn('userId', col('value').cast('integer')).\
+        drop('value')
+    rec = model.recommendForUserSubset(user_subset, 20) #model not loaded
+    movie_id_rec = rec.select("recommendations.movieIndex")
+    rec_list = movie_id_rec.collect()[0][0]
+    toreturn = []
+    for movie1_id in rec_list:
+        final = lookup.filter(lookup.movieIndex == movie1_id)
+        Done = final.select("movieID")
+        toreturn.append(Done.collect()[0][0])
+    return toreturn
