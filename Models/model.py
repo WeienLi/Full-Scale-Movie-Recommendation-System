bfc8ea0897ea689d2b172155cc746ba87ddd5497
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-10-01 21:58:15 -0400

    Cache and send results

--- Models/model.py
+++ Models/model.py
@@ -1,35 +1,19 @@
 import json
 import os
-from pyspark.ml.recommendation import ALSModel
-from pyspark import SparkContext
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
-from pyspark.sql.types import IntegerType
-import random
-
-sc = SparkContext()
-spark = SparkSession.builder.appName('Recommendations').getOrCreate()
 
 def recommendMovies(userID):
-    #load model
+    # load the cached recommendations JSON
     root = os.path.join(os.path.dirname(__file__))
-    model = ALSModel.load(os.path.join(root, 'ALS'))
-    # read the lookup table JSON
-    with open(os.path.join(root, 'lookuptable', 'lookuptable.json'), 'r') as f:
+    with open(os.path.join(root, 'cache', 'Recommendations.json'), 'r') as f:
+        recommendations = json.load(f)
+    
+    # Return a pre-defined (random) result if the user ID is not in the recommendations
+    if userID not in recommendations:
+        userID = 0
+    
+    rec_list = recommendations[str(userID)]
+    # load the cached lookup table JSON
+    with open(os.path.join(root, 'cache', 'lookuptable.json'), 'r') as f:
         lookup = json.load(f)
-    userid_int = int(userID)
-    user_subset = spark.createDataFrame([userid_int], IntegerType())
-    user_subset = user_subset. \
-        withColumn('userId', col('value').cast('integer')).\
-        drop('value')
-    try:
-        rec = model.recommendForUserSubset(user_subset, 20) #model not loaded
-        movie_id_rec = rec.select("recommendations.movieIndex")
-        rec_list = movie_id_rec.collect()[0][0]
-    except:
-        rec_list = random.sample(range(1, 25533), 20)
-    toreturn = []
-    for movie1_id in rec_list:
-        movie1_title = lookup[str(movie1_id)]
-        toreturn.append(movie1_title)
-    return toreturn
+    # return the movie titles
+    return [lookup[str(movieID)] for movieID in rec_list]
