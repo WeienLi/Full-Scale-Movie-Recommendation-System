26748bff5a512ae2605f1b33a73dd4cdca058fe8
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-10-21 02:58:17 -0400

    Docker setup for prometheus+grafana
    Configures project to use docker-compose to build&serve the API, Prometheus database, and Grafana service.

--- docker.sh
+++ docker.sh
@@ -2,37 +2,68 @@
 ## Use this script to start/stop the project in a Docker container
 
 cmd=$1
-image_tag="flask_api:1"
+flask_api_tag="flask_api"
 
-# Stop the flask API and remove any pre-existing Docker containers
+# Stop all existing Docker containers
 function stop() {
-    containers=$(docker ps -a -q --filter ancestor=$image_tag --format="{{.ID}}")
+    containers=$(docker ps -a -q)
     if [ -z "$containers" ]; then
         echo "No containers running"
     else
-        echo "Stopping containers with tag $image_tag..."
+        echo "Stopping containers..."
         docker stop $containers
-        docker rm $containers
     fi
 }
 
-# Start the flask API in a Docker container (background)
+# Create and start all Docker images and containers
 function start() {
-    if [ "$(docker images -q $image_tag 2> /dev/null)" != "" ]; then
+    containers=$(docker ps -a -q)
+    if [ -n "$containers" ]; then
+        flask_api_container=$(docker ps -a | grep $flask_api_tag | awk '{print $1}')
+        # remove flask_api container if it exists
+        if [ -n "$flask_api_container" ]; then
+            docker stop $flask_api_container
+            docker rm $flask_api_container
+        fi
+        flask_api_image=$(docker images | grep $flask_api_tag | awk '{print $3}')
+        # remove flask_api image if it exists
+        if [ -n "$flask_api_image" ]; then
+            docker rmi $flask_api_image
+        fi
         stop
-        echo "Removing the existing container..."
-        docker rmi $image_tag -f
     fi
-    echo "Building the image..."
-    docker build -t "$image_tag" .
-    echo "Starting the container..."
-    docker run -d -p 8082:8082 "$image_tag"
+
+    echo "Starting new containers..."
+    docker-compose up -d
+}
+
+# remove all Docker containers, images, and volumes
+function reset() {
+    # remove all containers if they exist
+    containers=$(docker ps -a -q)
+    if [ -n "$containers" ]; then
+        stop
+        docker rm $containers
+    fi
+    # remove all images if they exist
+    images=$(docker images -q)
+    if [ -n "$images" ]; then
+        docker rmi $images
+    fi
+    # remove all volumes if they exist
+    volumes=$(docker volume ls -q)
+    if [ -n "$volumes" ]; then
+        docker volume rm $volumes
+    fi
+    echo "All containers, images, and volumes removed"
 }
 
 if [ "$cmd" == "start" ]; then
     start
 elif [ "$cmd" == "stop" ]; then
     stop
+elif [ "$cmd" == "reset" ]; then
+    reset
 else
-    echo "Invalid command"
+    echo "Usage: docker.sh [start|stop|reset]"
 fi
