3b93c068787fdcd2fb4d568d3549d26b11234630
Shanzid Shaiham <shanzid.shaiham@gmail.com>
2022-11-13 19:15:10 -0500

    Improve online evaluations
    - Add the ability to distinguish between canary evaluation and main app online evaluation

--- kafka-consumer/monitor.py
+++ kafka-consumer/monitor.py
@@ -4,7 +4,7 @@ import time
 from Database.db import RedisDB
 
 from .utils.common import get_consumer, start_prometheus
-from .utils.constants import MessageType
+from .utils.constants import APP_MODE, MessageType
 from .utils.message_parser import parse_message
 
 start_prometheus()
@@ -13,6 +13,34 @@ db = RedisDB()
 consumer = get_consumer()
 
 print("Listening for Kafka messages...")
+
+
+def getAppModeFromUID(uid):
+    """Find which app mode (canary or main) was used to generate the latest recommendations
+    for the user. Returns 'canary' for Canary app, or 'main' for Main app recommendations."""
+    try:
+        # get data from recommendations cache
+        main_data = db.get(uid)
+        canary_data = db.get(APP_MODE.CANARY + ":" + uid)
+        if canary_data is not None and main_data is not None:
+            canary_data = json.loads(canary_data)
+            main_data = json.loads(main_data)
+            if "timestamp" in canary_data and "timestamp" in main_data:
+                if float(canary_data["timestamp"]) > float(main_data["timestamp"]):
+                    return APP_MODE.CANARY
+                else:
+                    return APP_MODE.MAIN
+            elif "timestamp" in canary_data:
+                return APP_MODE.CANARY
+            else:
+                return APP_MODE.MAIN
+        elif canary_data is not None:
+            return APP_MODE.CANARY
+        else:
+            return APP_MODE.MAIN
+    except Exception as e:
+        print("Error getting app mode from uid: ", e)
+        return APP_MODE.MAIN
 
 
 def process_message(message):
@@ -34,32 +62,46 @@ def process_message(message):
             {
                 "movies": movie_list,
                 "created_time": time.time(),
+                "app_mode": getAppModeFromUID(userId),
             }
         )
         db_key = "rec:" + userId
         db.set(db_key, value)
         return
 
-    if parsed_message[0] == MessageType.WATCHTIME:
+    elif parsed_message[0] == MessageType.BROKEN:
+        db.execute_command("TS.ADD", "rec:failed:counter", "*", 1)
+
+    elif parsed_message[0] == MessageType.WATCHTIME:
         userId = parsed_message[2]
         db_key = "rec:" + userId
         value = db.get(db_key)
         if value is not None:
             value = json.loads(value)
             recommended_movies = value["movies"].split(",")
+            db_key_prefix = ""
+            if "app_mode" in value and value["app_mode"] == APP_MODE.CANARY:
+                db_key_prefix = APP_MODE.CANARY + ":"
+
             movieId = parsed_message[3]
             if movieId in recommended_movies:
                 # increment the count for watched recommended movies
-                db.execute_command("TS.INCRBY", "rec:watched:counter", 1)
+                db.execute_command(
+                    "TS.INCRBY", db_key_prefix + "rec:watched:counter", 1
+                )
 
                 # add the time difference between the recommendation and the watchtime
                 time_diff = time.time() - value["created_time"]
-                db.execute_command("TS.ADD", "rec:watched:after", "*", time_diff)
+                db.execute_command(
+                    "TS.ADD", db_key_prefix + "rec:watched:after", "*", time_diff
+                )
 
                 # add the rank of the movie in the recommendation list
                 movie_index = recommended_movies.index(movieId) + 1
                 rank = 1 / movie_index
-                db.execute_command("TS.ADD", "rec:watched:rank", "*", rank)
+                db.execute_command(
+                    "TS.ADD", db_key_prefix + "rec:watched:rank", "*", rank
+                )
 
                 # remove the recommendation from the list of stored recommendations
                 new_recommendations = recommended_movies
@@ -68,8 +110,6 @@ def process_message(message):
                 ] = "#"  # replace the movieId with a # symbol
                 value["movies"] = ",".join(new_recommendations)
                 db.set(db_key, json.dumps(value))
-
-                print("logged watched")
 
 
 for message in consumer:
